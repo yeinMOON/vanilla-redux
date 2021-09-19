03ca413bb16c29d7f1a5a36955bf077e660090a6
yeinMOON <yeindianamoon@gmail.com>
2021-09-19 16:49:28 +0900

    make counter with Vanilla JS and Redux

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// counter with Vanilla JS
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count += 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+// counter with Vanilla JS & Redux
+import { createStore } from "redux";
+
+const minus = document.getElementById("minus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+numver.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+counsStore.subscribe(onChange);
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("clcik", handleAdd);
+minus.addEventListener("clcik", handleMinus);
