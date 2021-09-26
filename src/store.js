55224606c6a8acec43fce37a80fb6ee150f1627b
yeinMOON <yeindianamoon@gmail.com>
2021-09-26 15:33:12 +0900

    Connect with localStorage

--- src/store.js
+++ src/store.js
@@ -28,10 +28,19 @@ const reducer = (state = [], action) => {
   }
 };
 
-const store = createStore(reducer);
+const persistedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : [];
+
+const store = createStore(reducer, persistedState);
+
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 export const actionCreators = {
   addToDo,
   deleteToDo,
 };
+
 export default store;
